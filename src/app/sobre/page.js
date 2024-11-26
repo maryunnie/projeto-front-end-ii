693d9fe7895725190ba5be2491bf25f0cf733f8f
mariannygoncalvesmoreira@gmail.com <seuemail@gmail.com>
2024-11-26 20:19:17 -0300

    up

--- src/app/sobre/page.js
+++ src/app/sobre/page.js
@@ -4,16 +4,16 @@ export default function Sobre() {
   return (
     <main>
       <h1 className={styles.h1}>Frank Ocean Ivy</h1>
-      <p>Eu pensei que estava sonhando quando você disse que me amava</p>
-      <p>I thought that I was dreamin' when you said you love me</p>
-      <p>O começo de nada</p>
-      <p>The start of nothin'</p>
-      <p>Eu não tive chance pra me preparar, não consegui ver você chegando</p>
-      <p>I had no chance to prepare, I couldn't see you comin'</p>
-      <p>O começo de nada</p>
-      <p>The start of nothin'</p>
-      <p>Ooh, eu poderia te odiar agora</p>
-      <p>Ooh, I could hate you now</p>
+      <p className={styles.p}>Eu pensei que estava sonhando quando você disse que me amava</p>
+      <p className={styles.p}>I thought that I was dreamin' when you said you love me</p>
+      <p className={styles.p}>O começo de nada</p>
+      <p className={styles.p}>The start of nothin'</p>
+      <p className={styles.p}>Eu não tive chance pra me preparar, não consegui ver você chegando</p>
+      <p className={styles.p}>I had no chance to prepare, I couldn't see you comin'</p>
+      <p className={styles.p}>O começo de nada</p>
+      <p className={styles.p}>The start of nothin'</p>
+      <p className={styles.p}>Ooh, eu poderia te odiar agora</p>
+      <p className={styles.p}>Ooh, I could hate you now</p>
     </main>
   );
 }
